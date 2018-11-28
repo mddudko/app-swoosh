ab688cd9139f629286c9ecca24ff1d258ceda62b
Michal Dominik Dudko <md.dudko@icloud.com>
2018-11-28 11:03:47 +0100

    segue programmatically

---
+++ app-swoosh/app-swoosh/Controller/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  app-swoosh
+//
+//  Created by Michal Dudko on 28.11.18.
+//  Copyright © 2018 Michal Dudko. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
